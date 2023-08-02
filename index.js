03edbea26a4285efd6066fe1d95ce0a179c1060b
realapolloailus <apolloailus@gmail.com>
2023-08-02 16:27:00 +0300

    Did 3.1

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
 console.log('hello world')
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+
+app.get('/api/persons', (req, res) =>{
+    res.json(persons)
+})
+
+const generateID =  () =>{
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map( p => p.id ))
+        : 0
+    return maxId+1
+}
+
+app.post('/api/persons', (request, response)=>{
+    const body = request.body
+    if(!body.name){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateID(),
+    }
+    persons.concat(person)
+    response.json(person)
+
+})
+
+app.get('/api/persons/:id', (request, response)=>{
+    const id = Number(request.params.id)
+    const person = persons.find(p=> p.id===id)
+
+    if(person){
+        response.json(person)
+    }
+    else{
+        response.status(400).end()
+    }
+
+})
+
+app.delete('api/notes/:id', (request, response)=>{
+    const id =Number(request.params.id)
+    person = persons.filter(p=> p.id!==id)
+    response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
