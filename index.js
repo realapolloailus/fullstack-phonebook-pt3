46706c75964e80790a986bc9d1d42903d7704903
realapolloailus <apolloailus@gmail.com>
2023-08-05 12:33:45 +0300

    Did 3.2/3.3, 3.4 in progress

--- index.js
+++ index.js
@@ -7,30 +7,32 @@ app.use(express.json())
 
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
   })
+
+// Begin code for api/persons:  
 
 app.get('/api/persons', (req, res) =>{
     res.json(persons)
@@ -45,39 +47,58 @@ const generateID =  () =>{
 
 app.post('/api/persons', (request, response)=>{
     const body = request.body
-    if(!body.name){
+    if(!body.name || !body.number){
         return response.status(400).json({
             error: 'content missing'
         })
     }
     const person = {
+        id: generateID(),
         name: body.name,
         number: body.number,
-        id: generateID(),
     }
-    persons.concat(person)
+    persons = persons.concat(person)
     response.json(person)
 
 })
 
 app.get('/api/persons/:id', (request, response)=>{
     const id = Number(request.params.id)
-    const person = persons.find(p=> p.id===id)
+    console.log(id);
+    const person = persons.find(p=> p.id === id)
+    console.log(person);
 
     if(person){
         response.json(person)
     }
     else{
-        response.status(400).end()
+        response.status(404).end()
     }
 
 })
 
-app.delete('api/notes/:id', (request, response)=>{
+app.delete('api/persons/:id', (request, response)=>{
     const id =Number(request.params.id)
-    person = persons.filter(p=> p.id!==id)
+    console.log(id);
+    persons = persons.filter(p=> p.id !== id)
+    console.log(persons);
+
     response.status(204).end()
 })
+
+// End code for api/persons.
+
+// Begin code for /info:
+
+
+app.get('/info', (request, response)=>{
+    var date = new Date();
+    const totalLen=persons.length
+    response.send(`<p>Phonebook has info for ${totalLen} people</p>
+                   <p>${date}</p>`)
+})
+
+// End code for api/info.
 
 const PORT = 3001
 app.listen(PORT, () => {
