8667947452bbeaee3e5c2a3ed8be117f44659a0e
realapolloailus <apolloailus@gmail.com>
2023-08-08 14:33:01 -0400

    Copied build repository

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('content', (request) =>
   request.method === 'POST' && request.body.name
