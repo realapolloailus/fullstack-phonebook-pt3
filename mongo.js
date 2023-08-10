4481ec88faac5994129506583489f81f3f92caee
realapolloailus <apolloailus@gmail.com>
2023-08-10 15:06:21 -0400

    Did 3.12

---
+++ mongo.js
@@ -0,0 +1,55 @@
+// Practice file for MongoDB functionality.
+
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+
+
+const yourpassword = process.argv[2]
+
+const url =
+  `mongodb+srv://apolloailus:${'bTSarK69Fyn0LngB'}@cluster0.xmyde6z.mongodb.net/personApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+
+
+if (process.argv.length===3) {
+    console.log('phonebook:');
+    Person
+        .find({ })
+        .then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+    mongoose.connection.close()
+    })
+}
+
+
+else{
+    person.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+    })
+}  
+
+
+
