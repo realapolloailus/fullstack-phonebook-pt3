3d61a03668c38adbf8fa9185b1a222fccf1f1a9e
realapolloailus <apolloailus@gmail.com>
2023-08-22 12:41:18 +0300

    Added custom validator

--- mongo.js
+++ mongo.js
@@ -24,11 +24,20 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
     name:{
       type: String,
-      minLength: 3
+      minLength: 3,
+      required: true
     },
     
     number:{
-      type: String
+      type: String,
+      minLength: 8,
+      validate: {
+        validator: function(v) {
+          return /^\d{2,3}-\d{7,}$/g.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      required: true
     } 
 })
 
