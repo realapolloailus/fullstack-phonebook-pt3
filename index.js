1c3e14954ac748581381a815eb61786134d31c06
realapolloailus <apolloailus@gmail.com>
2023-08-22 09:28:31 +0300

    Tweaks to 3.15

--- index.js
+++ index.js
@@ -37,22 +37,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :c
 app.use(cors())
 app.use(express.static('build'))
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) =>{
-  console.error(error.message)
-  if(error.name==='CastError'){
-    return response.status(400).send({
-      error: 'malformatted id'
-    })
-  }
-  next(error)
-}
-app.use(errorHandler)
-
 
 morgan.token('content', (request) =>
   request.method === 'POST' && request.body.name
@@ -175,13 +159,33 @@ app.get('/info', (request, response)=>{
 
 // Begin code for deleting phonebook entries:
 app.delete('/api/persons/:id', (request, response, next ) =>{
-  Person.findByIdAndRemove(request.params.id)
+  Person.findOneAndDelete({id: request.params.id})
     .then(result => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) =>{
+  console.error(error.message)
+  if(error.name==='CastError'){
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  }
+  else if(error.name === 'ValidationError'){
+    return response.status(400).json({ 
+      error: error.message 
+    })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
