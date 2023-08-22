e06a1617bf06395803de56240ee317ffe3c1e8e1
realapolloailus <apolloailus@gmail.com>
2023-08-22 13:01:21 +0300

    Attempt 3.20

--- mongo.js
+++ mongo.js
@@ -33,7 +33,7 @@ const personSchema = new mongoose.Schema({
       minLength: 8,
       validate: {
         validator: function(v) {
-          return /^\d{2,3}-\d{7,}$/g.test(v);
+          return /^\d{2,3}-\d{7,}$/.test(v);
         },
         message: props => `${props.value} is not a valid phone number!`
       },
